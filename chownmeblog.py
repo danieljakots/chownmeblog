07ff2a991a3580d22af7dea521195fc9298f8b19
Daniel Jakots <danj@chown.me>
2020-04-10 21:30:19 -0400

    This domain makes more sense

--- chownmeblog.py
+++ chownmeblog.py
@@ -17,7 +17,7 @@ CONTENT_PATH = "content/*"
 SITE = {}
 SITE["author"] = "Daniel Jakots"
 SITE["name"] = SITE["author"]
-SITE["url"] = "https://chown.me"
+SITE["url"] = "https://pre-prod.chown.me"
 OUTPUT_DIR = "output"
 
 
